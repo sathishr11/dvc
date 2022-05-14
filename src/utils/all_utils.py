63178907e0ee6b9d96df224ceac80d367419bb9d
Sathish <mail2sathish11@gmail.com>
2022-05-14 18:58:47 +0530

    added function to create directory

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -6,3 +6,8 @@ def read_yaml(path_to_yaml: str)-> dict:
         content = yaml.safe_load(yaml_file)
 
     return content
+
+def create_directory(dirs:list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f'Directory {dir_path} created')
